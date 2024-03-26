445e3129baa49d1a0dab49f7c5958388812beb51
s28977 <s28977@pjwstk.edu.pl>
2024-03-26 09:28:11 +0100

    Solved task 1

---
+++ Lab3/tasks.py
@@ -0,0 +1,6 @@
+# Task 1: List Comprehensions
+# Write a Python program that generates a list of squares of numbers from 1 to 10 using list
+# comprehensions.ares)
+
+squares = [x ** 2 for x in range(1, 10)]
+print(squares)

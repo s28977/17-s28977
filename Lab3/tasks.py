ef42bb23f480bf861682e7032ec2fc4a62134dd0
s28977 <s28977@pjwstk.edu.pl>
2024-03-26 09:34:54 +0100

    Solved task 2: square function

--- Lab3/tasks.py
+++ Lab3/tasks.py
@@ -1,6 +1,18 @@
 # Task 1: List Comprehensions
 # Write a Python program that generates a list of squares of numbers from 1 to 10 using list
-# comprehensions.ares)
+# comprehensions
 
 squares = [x ** 2 for x in range(1, 10)]
 print(squares)
+
+
+# Task 2: Functions
+# Expand the previous program by defining a function that takes a range of numbers as input
+# and returns a list of squares for that range.e_squares(start, end))
+
+def squares(start, end):
+    return [x ** 2 for x in range(start, end)]
+
+
+print(squares(2, 8))
+
